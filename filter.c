1eed9095c44b4f4a6960742f6b6bf734d3d7e659
Connor Lynch <vahid.ramezani.2014@gmail.com>
2021-05-06 23:52:18 +0430

    bugs fixed anf filter.c added

---
+++ filter.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+const int KERNEL_SIZE=3;
+const int IM_SIZE=512;
+
+void generate_kernel(int kernel[KERNEL_SIZE][KERNEL_SIZE]){
+
+}
+
+
+void main(int argc, char* argv[]){
+    char* file_addr;
+	char* fifo_name;
+	int fd;
+	int image[IM_SIZE][IM_SIZE];
+    int kernel[KERNEL_SIZE][KERNEL_SIZE];
+
+    if(argc<2){
+        fputs("Eror: too few arguments. (histogram_calculator)\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    fifo_name=argv[1];
+
+	mkfifo(fifo_name,0666);
+
+	fd=open(fifo_name,O_RDONLY);
+
+
+}
